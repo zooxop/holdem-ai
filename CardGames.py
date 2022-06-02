65070f2d0caf274a9175ee25f3066c6625914e06
Parker <hak891@gmail.com>
2022-06-02 20:27:16 +0900

    play two player

--- CardGames.py
+++ CardGames.py
@@ -56,6 +56,7 @@ class Player():
         self.hand = [-1,-1,-1]
         self.combine = Combination.TOP
         self.top = -1
+        self.betMoney = 0
 
     def GetCombine(self):
         hand_temp = self.hand
@@ -80,32 +81,56 @@ class Player():
         if(value > self.chip):
             return 0
         self.chip -= value
+        self.betMoney += value
         return value
 
     def PrintCards(self):
         print("public:{}, {}".format(Cards.Card(self.hand[0]), Cards.Card(self.hand[1])))
         print("opponent:{}".format(Cards.Card(self.hand[2])))
 
+    def Win(self):
+        self.chip += self.betMoney * 2
+        self.betMoney = 0
+
+    def Defeat(self):
+        self.betMoney = 0
+
+class HoldemMaster():
+    def __init__(self):
+        self.deck = Cards.StandardDeck()
+        self.rate : float = 0.0
+        self.myCharacter = Player(30)
+
 class GameLogic():
     def __init__(self):
         self.deck = Cards.StandardDeck()
         self.deck.shuffle()
-        self.betMoney = 0
-        self.carriedMoney = 0
+        self.round = 1
+        self.player1 = Player(30)
+        self.player2 = Player(30)
 
-    def DealCard(self, p1 : Player, p2 : Player):
+    def ReloadDeck(self):
+        self.deck = Cards.StandardDeck()
+        self.deck.shuffle()
+
+    def DealCard(self):
         if(len(self.deck) == 0):
             self.ReloadDeck()
         card1 : Cards.Card = self.deck.deal()
         card2 : Cards.Card = self.deck.deal()
-        p1.hand = [card1.value, card2.value, self.deck.deal().value]
-        p2.hand = [card1.value, card2.value, self.deck.deal().value]
+        self.player1.hand = [card1.value, card2.value, self.deck.deal().value]
+        self.player2.hand = [card1.value, card2.value, self.deck.deal().value]
 
     def PrintStatus(self, p1 : Player):
         print("-----------------------------------")
         print("Opponent:", Cards.Card(p1.hand[2]))
         print("public:", Cards.Card(p1.hand[0]), Cards.Card(p1.hand[1]))
         print("-----------------------------------")
+
+    def PrintRoundInfo(self):
+        print("Round: ", self.round)
+        print("Player1:", self.player1.chip, "Player2:", self.player2.chip)
+        print("Base:", self.player1.betMoney + self.player2.betMoney)
 
     def GetMatch(self, p1 : Player, p2 : Player):
         p1.GetCombine()
@@ -122,25 +147,133 @@ class GameLogic():
         elif(p1.combine.value < p2.combine.value):
             return 1
 
-    def RoundResult(self, p1, p2, money):
-        result = self.GetMatch(p1, p2)
-        if(result == -1):
-            print("Player 1 won!")
-            p1.chip += money
-            self.carriedMoney=0
-            return 0
-        elif(result == 0):
-            print("Draw")
-            return money
-        elif(result == 1):
-            print("Player 2 won!")
-            p2.chip += money
-            self.carriedMoney = 0
-            return 0
+    def SelectBet(self, num : int) -> bool:
+        DrawLine()
+        #Player1의 배팅 페이즈
+        if(num == 1):
+            print("Player1's turn")
+            self.player2.PrintCards()
 
-    def ReloadDeck(self):
-        self.deck = Cards.StandardDeck()
-        self.deck.shuffle()
+            print("1. Bet\n2. call\n3. fold")
+            x = int(input("선택:"))
+
+            if(x == 1):
+                while(1):
+                    coin = int(input("베팅: "))
+                    if(coin > self.player1.chip or coin > self.player2.chip):
+                        print("다시 입력")
+                    else:
+                        self.player1.BetChip(coin)
+                        break
+
+                return True
+            elif(x == 2):
+                temp = self.player2.betMoney - self.player1.betMoney
+                self.player1.BetChip(temp)
+
+                return True
+            elif(x == 3):
+                self.player1.GetCombine()
+                if(self.player1.combine.value >= Combination.STRAIGHT.value):
+                    self.player1.BetChip(10)
+
+                return False
+        #Player2의 배팅 페이즈
+        elif(num == 2):
+            print("Player2's turn")
+            self.player1.PrintCards()
+
+            print("1. Bet\n2. call\n3. fold")
+            x = int(input("선택:"))
+
+            if (x == 1):
+                while (1):
+                    coin = int(input("베팅: "))
+                    if (coin > self.player1.chip or coin > self.player2.chip):
+                        print("다시 입력")
+                    else:
+                        self.player2.BetChip(coin)
+                        break
+
+                return True
+            elif (x == 2):
+                temp = self.player1.betMoney - self.player2.betMoney
+                self.player2.BetChip(temp)
+
+                return True
+            elif (x == 3):
+                self.player2.GetCombine()
+                if (self.player2.combine.value >= Combination.STRAIGHT.value):
+                    self.player2.BetChip(10)
+
+                return False
+
+    def RoundProcess(self) -> bool:
+        #라운드가 홀수면 Player1 선 배팅
+        #라운드가 짝수면 Player2 선 배팅
+        if(self.round % 2 == 1):
+            while(1):
+                if(self.SelectBet(1) == False):
+                    return False
+                if(self.SelectBet(2) == False):
+                    return False
+                if(self.player1.betMoney == self.player2.betMoney):
+                    return True
+        else:
+            while (1):
+                if (self.SelectBet(2) == False):
+                    return False
+                if (self.SelectBet(1) == False):
+                    return False
+                if (self.player1.betMoney == self.player2.betMoney):
+                    return True
+
+    def FinishRound(self):
+        result = self.GetMatch(self.player1, self.player2)
+        if(result == -1):
+            #player1 승
+            print("Player1 Won")
+            self.player1.Win()
+            self.player2.Defeat()
+        elif (result == 1):
+            # player2 승
+            print("Player2 Won")
+            self.player2.Win()
+            self.player1.Defeat()
+
+        self.round += 1
+
+    def Start(self):
+
+        while(1):
+            DrawLine()
+            if (self.player1.betMoney <= 0):
+                if (self.player1.chip <= 0):
+                    print("Player2 won")
+                    break
+            if (self.player2.betMoney <= 0):
+                if (self.player2.chip <= 0):
+                    print("Player1 won")
+                    break
+            if(not(self.player1.betMoney and self.player2.betMoney)):
+                self.player1.BetChip(1)
+                self.player2.BetChip(1)
+
+            #보유 칩 출력
+            self.PrintRoundInfo()
+            self.DealCard()
+
+            #라운드 진행
+            result = self.RoundProcess()
+
+            #라운드 종료 후 돈 배분
+            if(result):
+                self.FinishRound()
+            else:
+                print("Draw")
+
+
+
 
 while(1):
     DrawLine()
@@ -150,81 +283,8 @@ while(1):
     x = int(input("선택:"))
 
     if(x == 1):
-        DrawLine()
-        money = int(input("시작 코인:"))
         game = GameLogic()
-        #p1: 상대 p2: 나(플레이어)
-        p1 = Player(money)
-        p2 = Player(money)
-        while(1):
-            DrawLine();
-            if(game.carriedMoney == 0):
-                if(p1.chip <= 0):
-                    print("Player2 won")
-                    break
-                if (p2.chip <= 0):
-                    print("Player1 won")
-                    break
-                game.betMoney = p1.BetChip(1) + p2.BetChip(1)
-            else:
-                game.betMoney = game.carriedMoney
-                game.carriedMoney = 0
-
-            #보유 Chip 출력
-            print("player1:", p1.chip, "player2:", p2.chip)
-            print("Base:", game.betMoney)
-
-            #카드 분배
-            game.DealCard(p1, p2)
-
-            #상대 카드 출력
-            p1.PrintCards()
-
-            print("1. Bet\n2. call\n3. fold")
-            x = int(input("선택:"))
-
-            while(1):
-                if(x == 1):
-                    #금액 베팅
-                    while(1):
-                        #베팅 금액 입력
-                        money = int(input("Bet Chip:"))
-                        # 내 칩이나 상대 칩이 모자르면 다시 입력
-                        if(p1.chip < money or p2.chip < money):
-                            max : int = 0
-                            if(p1.chip < p2.chip):
-                                max = p1.chip
-                            else:
-                                max = p2.chip
-                            print("No chips, try again(max value:{})".format(max))
-                        else:
-                            game.betMoney += p1.BetChip(money) + p2.BetChip(money)
-                            break
-                    # 원래는 GetMatch도 하지말고 상대가 call 하면 GetMatch,break 해야 하는데 컴퓨터랑 하니깐 일단 한번만 돌려요
-                    print("Player 1:")
-                    p1.PrintCards()
-                    print("Player 2:")
-                    p2.PrintCards()
-                    game.RoundResult(p1, p2, game.betMoney)
-                    break
-                elif(x == 2):
-                    #비교 후 종료
-                    print("Player 1:")
-                    p1.PrintCards()
-                    print("Player 2:")
-                    p2.PrintCards()
-                    game.RoundResult(p1, p2, game.betMoney)
-                    break
-                elif(x == 3):
-                    if(p2.GetCombine() >= Combination.STRAIGHT):
-                        p2.chip -= 10
-                        game.betMoney += 10
-
-                    p1.chip += game.betMoney
-                    game.betMoney = 0
-                    break
-
-
+        game.Start()
 
     elif(x==2):
         break
